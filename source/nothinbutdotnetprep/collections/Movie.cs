b4419563ddbd21c0f72f9034f4c057c6a741dd6f
Bryan Horner <brhorner@bellsouth.net>
2011-09-19 10:41:41 -0500

    First Work

--- source/nothinbutdotnetprep/collections/Movie.cs
+++ source/nothinbutdotnetprep/collections/Movie.cs
@@ -2,12 +2,31 @@ using System;
 
 namespace nothinbutdotnetprep.collections
 {
-  public class Movie
+  public class Movie : IComparable<Movie>
   {
     public string title { get; set; }
     public ProductionStudio production_studio { get; set; }
     public Genre genre { get; set; }
     public int rating { get; set; }
     public DateTime date_published { get; set; }
+
+      #region Implementation of IComparable<in Movie>
+
+      /// <summary>
+      /// Compares the current object with another object of the same type.
+      /// </summary>
+      /// <returns>
+      /// A value that indicates the relative order of the objects being compared. The return value has the following meanings: Value Meaning Less than zero This object is less than the <paramref name="other"/> parameter.Zero This object is equal to <paramref name="other"/>. Greater than zero This object is greater than <paramref name="other"/>. 
+      /// </returns>
+      /// <param name="other">An object to compare with this object.</param>
+      public int CompareTo(Movie other)
+      {
+          if (other == null) return 1;
+          else if (this.title == other.title)
+              return 0;
+          else return -1;
+      }
+
+      #endregion
   }
 }
